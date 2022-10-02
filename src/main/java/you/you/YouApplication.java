6c2d124230b891c4272573c943a11a0df35d887a
jooomOut <rimeilo324@naver.com>
2022-10-02 23:42:32 +0900

    initial commit

---
+++ src/main/java/you/you/YouApplication.java
@@ -0,0 +1,13 @@
+package you.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/you/you/YouApplicationTests.java
@@ -0,0 +1,13 @@
+package you.you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
